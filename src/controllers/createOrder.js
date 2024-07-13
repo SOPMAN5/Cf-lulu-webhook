a016319d3e02dbfe75879536feae18bfd3646f1b
David Ahukanna <ahukannadavid5@gmail.com>
2024-07-13 20:22:13 +0100

    updated allowed product names

--- src/controllers/createOrder.js
+++ src/controllers/createOrder.js
@@ -10,14 +10,15 @@ const createPrintJobs = async (req, res) => {
 	const data =  req.body ;
 	const funnelPageId = req.body.data.attributes.order_page.id;
 	const allowedFunnelPages = [9240009,7083817,9241424,12538942];
+	const allowedProductNames = ['1:1 Coaching call','Hard Copy'];
     const productName = req.body.included.filter(
 		(e) => e.type === "orders/line_item"
 	)[0];
 	
-   const isValidProductName = productName.attributes.original_product_name.includes('Hard Copy');   
+   const isValidProductName = allowedProductNames.includes(productName.attributes.original_product_name);   
 	const isValidFunnelPage = allowedFunnelPages.includes(funnelPageId);
      
-	if(!isValidFunnelPage || !isValidProductName) return res.status(200).json({status:true,message:'Not matching funnel page'}); 
+	if(!isValidFunnelPage || !isValidProductName) return res.status(404).json({status:true,message:'Not matching funnel page'}); 
 	const result = await printJobs.createPrintJobs(data);
 	const statusCode = result.status ? 200 : 400;
 	return res.status(statusCode).json(result);
